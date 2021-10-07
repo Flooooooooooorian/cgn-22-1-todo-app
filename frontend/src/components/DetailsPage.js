325b68bc3d44a34ce578b4d955decce993c2d7f2
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-10-07 10:50:26 +0200

    Add DetailsPage.js

--- frontend/src/components/DetailsPage.js
+++ frontend/src/components/DetailsPage.js
@@ -1,12 +1,36 @@
 import {useParams} from "react-router-dom";
+import {getTodoBy} from "../service/todo-api-service";
+import {useEffect, useState} from "react";
+import styled from "styled-components/macro";
 
 export default function DetailsPage() {
 
+    const [todo, setTodo] = useState({})
     const {id} = useParams()
 
+    useEffect(() => {
+        getTodoBy(id)
+            .then(response => response.data)
+            .then(data => setTodo(data))
+            .catch(error => console.error(error))
 
+    }, [id])
 
     return (
-        <p>{id}</p>
+        <Wrapper>
+            <h2>TODO</h2>
+            <SubTitle>{todo.status}  ({todo.id}) </SubTitle>
+            <p>{todo.description}</p>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    text-align: center;
+    margin: 12px;
+`
+
+const SubTitle = styled.p`
+    font-style: italic;
+    font-size: 90%;
+`
