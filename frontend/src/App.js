6164068d6a242fdcccd1e22f7dca86fba5195aee
Matthias Hiemer <matthias.hiemer@neuefische.de>
2021-10-07 15:23:40 +0200

    Add prop types for all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,49 @@
-import Header from "./components/Header";
-import styled from "styled-components/macro";
+import Header from './components/Header'
+import styled from 'styled-components/macro'
 
-import NavigationBar from "./components/NavigationBar";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Homepage from "./components/Homepage";
-import BoardPage from "./components/BoardPage";
-import DetailsPage from "./components/DetailsPage";
-import useTodos from "./hooks/useTodos";
+import NavigationBar from './components/NavigationBar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Homepage from './components/Homepage'
+import BoardPage from './components/BoardPage'
+import DetailsPage from './components/DetailsPage'
+import useTodos from './hooks/useTodos'
 
 function App() {
+  const { todos, addTodo, advanceTodo, removeTodo } = useTodos()
 
-    const {todos, addTodo, advanceTodo, removeTodo} = useTodos()
-
-    return (
-        <Router>
-            <PageLayout>
-                <Header/>
-                <NavigationBar/>
-                <Switch>
-                    <Route path="/" exact>
-                        <Homepage
-                            todos={todos}
-                            onAdvance={advanceTodo}
-                            onDelete={removeTodo}
-                            onAdd={addTodo}
-                        />
-                    </Route>
-                    <Route path="/todos/:statusSlug">
-                        <BoardPage
-                            todos={todos}
-                            onAdvance={advanceTodo}
-                            onDelete={removeTodo}
-                        />
-                    </Route>
-                    <Route path={"/todo/:id"}>
-                        <DetailsPage/>
-                    </Route>
-                </Switch>
-            </PageLayout>
-        </Router>
-    );
+  return (
+    <Router>
+      <PageLayout>
+        <Header />
+        <NavigationBar />
+        <Switch>
+          <Route path="/" exact>
+            <Homepage
+              todos={todos}
+              onAdvance={advanceTodo}
+              onDelete={removeTodo}
+              onAdd={addTodo}
+            />
+          </Route>
+          <Route path="/todos/:statusSlug">
+            <BoardPage
+              todos={todos}
+              onAdvance={advanceTodo}
+              onDelete={removeTodo}
+            />
+          </Route>
+          <Route path={'/todo/:id'}>
+            <DetailsPage />
+          </Route>
+        </Switch>
+      </PageLayout>
+    </Router>
+  )
 }
 
-export default App;
+export default App
 
 const PageLayout = styled.div`
-
   position: fixed;
   top: 0;
   left: 0;
--- frontend/src/service/todo-api-service.js
+++ frontend/src/service/todo-api-service.js
@@ -1,36 +1,29 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const getTodos = () => {
   return axios
-    .get("/api/todo")
-    .then((response) => response.data)
-    .catch((error) => console.log(error));
-};
+    .get('/api/todo')
+    .then(response => response.data)
+    .catch(error => console.log(error))
+}
 
-export const postTodo = (newDescription) => {
+export const postTodo = newDescription => {
   const newTodo = {
     description: newDescription,
-    status: "OPEN",
-  };
+    status: 'OPEN',
+  }
 
-  return axios
-      .post("/api/todo", newTodo)
-      .then((response) => response.data);
-};
+  return axios.post('/api/todo', newTodo).then(response => response.data)
+}
 
-export const putTodo = (todo) => {
-  return axios
-    .put(`/api/todo/${todo.id}`, todo)
-    .then((response) => response.data);
-};
+export const putTodo = todo => {
+  return axios.put(`/api/todo/${todo.id}`, todo).then(response => response.data)
+}
 
-export const deleteTodo = (id) => {
-  return axios
-      .delete(`/api/todo/${id}`);
-};
+export const deleteTodo = id => {
+  return axios.delete(`/api/todo/${id}`)
+}
 
-
-export const getTodoBy = (id) => {
-  return axios
-      .get(`/api/todo/${id}`)
+export const getTodoBy = id => {
+  return axios.get(`/api/todo/${id}`)
 }
