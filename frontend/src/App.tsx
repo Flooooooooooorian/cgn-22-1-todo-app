14dbabc24066b3e464c16d560c7435f6bf97efb8
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-22 10:49:02 +0200

    Prototype dummy Routes to details and Board.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import BoardPage from "./page/BoardPage";
+import Details from "./component/Details";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<BoardPage />} path={"/"}/>
+                <Route element={<Details />} path={"/details"}/>
+            </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
