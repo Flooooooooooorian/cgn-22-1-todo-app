750f9edddb5b85c9da792dfe7d04b90098e06c69
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-05-04 09:12:33 +0200

    Add WhoAmI Endpoint

---
+++ backend/src/main/java/de/neuefische/backend/dto/AppUserDto.java
@@ -0,0 +1,13 @@
+package de.neuefische.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUserDto {
+
+    private String username;
+}
