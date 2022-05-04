3e920d7440a37a4f869ded91b826c6295e41ab07
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-05-04 14:55:34 +0200

    Revert "Add JWTFilter"
    This reverts commit 910373734c35217821a6ab8fb936d9c5ccd0c565.

--- backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package de.neuefische.backend.security.config;
 
-import de.neuefische.backend.security.filter.JwtAuthFilter;
 import de.neuefische.backend.security.service.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,20 +7,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AppUserDetailsService appUserDetailsService;
-    private final JwtAuthFilter jwtAuthFilter;
 
-    public SecurityConfig(AppUserDetailsService appUserDetailsService, JwtAuthFilter jwtAuthFilter) {
+    public SecurityConfig(AppUserDetailsService appUserDetailsService) {
         this.appUserDetailsService = appUserDetailsService;
-        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Override
@@ -44,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
                 .antMatchers("/**").authenticated()
-                .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().formLogin()
+                .and().httpBasic();
     }
 }
